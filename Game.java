f82b39a1ce0fd80f9baad1d2ef11c924f8a34f96
Noli3z <n.noppadolwilai@gmail.com>
2024-09-29 00:31:44 +0700

    Add files via upload

--- Game.java
+++ Game.java
@@ -1,69 +1,44 @@
+package main;
+
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 public class Game extends JFrame {
-    JButton button;
-    Timer timer;
-    int buttonY = 0;
-    final int STEP = 3;
-    final int DELAY = 40; 
-    JButton[] binary = new JButton[8];
-    int over = 394;
-    int y = 300;
-    static int x = 5;
+    public MenuPanel menuPanel;
+    public GamePanel gamePanel;
+    public GameLogic gameLogic;
 
-    public Game() {
+    public Game(){
 
-        setTitle("8bit");
-        setSize(460, 600);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLayout(null);
+        setTitle("BIT^3");
+        setLayout(new CardLayout());
+        menuPanel = new MenuPanel(this);
+        add(menuPanel,"Menu");
+        gamePanel = new GamePanel(this);
+        gameLogic = gamePanel.gameLogic;
+        add(gamePanel,"Game");
 
-        button = new JButton("2B");
-        button.setBounds(150, buttonY, 100, 50);
-        button.setEnabled(false);
-        add(button);tt
-        JButton line = new JButton("");
-        line.setBounds(0, 440, 460, 5);
-        line.setEnabled(false);
+        switchToMenu();
 
-
-        add(line);
-        for(int i = 0 ; i < 8 ; i++){
-            binary[i] = new JButton("0");
-            binary[i].setBounds(x, 500, 50, 50);
-            x+=55;
-            add(binary[i]);
-        } 
-        
-
-        timer = new Timer(DELAY, new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                buttonY += STEP;
-                if (buttonY >= y) {
-                     
-                    button = null;
-                    
-                }
-                if (buttonY >= over) {
-                    
-                    timer.stop();
-                    JOptionPane.showMessageDialog(null, "Game Over!");  // แสดงข้อความ Game Over
-                }
-                button.setBounds(150, buttonY, 100, 50);
-            }
-        });
-        timer.start();
+        pack();
+        setResizable(false);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setVisible(true);
     }
-    
+
+    public void switchToMenu(){
+        CardLayout layout = (CardLayout) this.getContentPane().getLayout();
+        layout.show(this.getContentPane(), "Menu");
+    }
+
+    public void switchToGame() {
+        CardLayout layout = (CardLayout) this.getContentPane().getLayout();
+        layout.show(this.getContentPane(), "Game");
+    }
 
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> {
-            Game bit = new Game();
-            bit.setVisible(true);
-        });
+        new Game();
     }
+
 }
